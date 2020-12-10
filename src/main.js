ada64860d5e5f7f7d17f51dd1ff3a4440141e430
CharlyLuna <cluna10@ucol.mx>
2020-12-10 15:44:35 -0600

    Implemented factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-export default class App {}
+export default class App {
+
+    factorial(numero) {
+        let factor = 1;
+        for(let i = 1;i <= numero; i++ ) {
+            factor = factor * i;
+        }
+        return factor;
+    }
+}
 
 let app = new App();
+
+//Comprobacion factorial
+console.log(app.factorial(5));
